4aaf7f25eac669cafbdb3bcdc79a007d9b2d0db8
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-07-27 16:33:27 +0900

    #voicesの実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
   $('.voices__js-slide').slick({
     arrows: false,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    centerMode: true,
+    centerMode: false,
     autoplay: false,
     infinite: true,
     speed: 300,
     variableWidth: true,
     dots: true,
-    dotsClass: 'voices__dots'
+    dotsClass: 'voices__dots',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          variableWidth: true
+        }
+      }
+    ]
   });
 });
